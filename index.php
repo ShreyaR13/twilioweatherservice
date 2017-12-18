864eeb868a5a683244b13cc0f37f3c767f6d3e4f
ShreyaR13 <s4.shre@gmail.com>
2017-12-18 17:23:21 -0500

    commit 1

--- index.php
+++ index.php
@@ -2,18 +2,18 @@
     require_once 'twilio-php-master/Twilio/autoload.php'; // Loads the library
     require 'twilio-php-master/Services/Twilio.php';
     use Twilio\Rest\Client;
-    $account_sid = 'ACc2ee9a0b7bd01ddfd3079fc23a433407'; 
-    $auth_token = '6a7fed7f68a9d89d75996bc165982d2b'; 
-    $client = new Client($account_sid, $auth_token); 
+    $account_sid = 'ACc2ee9a0b7bd01ddfd3079fc23a433407';
+    $auth_token = '6a7fed7f68a9d89d75996bc165982d2b';
+    $client = new Client($account_sid, $auth_token);
 
 $number = $_POST['From'];
 $body = $_POST['Body'];
-$url = 'api.openweathermap.org/data/2.5/weather?q=city.name&APPID=ef77ade3db3ce484b43606e2095ff696';
+$url = 'api.openweathermap.org/data/2.5/weather?q=London&APPID=ef77ade3db3ce484b43606e2095ff696';
 $file = file_get_contents($url);
 $data = json_decode($file, true);
 global $city.name;
 
-foreach ($data as $character) {  
+foreach ($data as $character) {
         if($character['city.name'] == $body) {
           $temp_min = $character['temp_min'];
           $temp_max = $character['temp_max'];
@@ -27,9 +27,9 @@ foreach ($data as $character) {
             "+12674407881",
             // Step 6: Set the URL Twilio will request when the call is answered.
            //array("url" => "http://demo.twilio.com/welcome/voice/")
-           array("url" => "https://troubled-gun-1513.twil.io/assets/voice.php")   
+           array("url" => "https://troubled-gun-1513.twil.io/assets/voice.php")
         );
-                
+
     } catch (Exception $e) {
         echo "Error: " . $e->getMessage();
     }
--- voice.php
+++ voice.php
@@ -5,14 +5,14 @@
     $body = $_POST['Body'];
     $url = 'api.openweathermap.org/data/2.5/weatherq=city.name&APPID=ef77ade3db3ce484b43606e2095ff696';
     $file = file_get_contents($url);
-        foreach ($data as $character) {  
+        foreach ($data as $character) {
         if($character['city.name'] == $body) {
           $temp_min = $character['temp_min'];
           $temp_max = $character['temp_max'];
         echo "<Response>
             <Say>
-                   Minimum Temperateure of " . $city.name . " is " . $temp_min . "
-                   Maximum Temperateure of " . $city.name . " is " . $temp_max . "
+                   Minimum Temperature of " . $city.name . " is " . $temp_min . "
+                   Maximum Temperature of " . $city.name . " is " . $temp_max . "
             </Say>
         </Response>
         ";
