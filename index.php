2c161af3b08ef55a8787661d48a988d97e7db07e
Shreya Rajwade <s4.shre@gmail.com>
2017-12-19 20:28:40 -0500

    Commit 17

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
         $call = $client->account->calls->create(
             // Step 4: Change the 'To' number below to whatever number you'd like
             // to call.
-            "+12016809992",
+            $number,
 
             // Step 5: Change the 'From' number below to be a valid Twilio number
             // that you've purchased or verified with Twilio.
