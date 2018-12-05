8d64149bd6552b440169fbbb406b26ea5b54604f
Shreya Rajwade <s4.shre@gmail.com>
2018-12-04 19:08:28 -0500

    Update index_backup.php
    Auth tokens removed

--- index_backup.php
+++ index_backup.php
@@ -2,8 +2,8 @@
     require_once 'twilio-php-master/Twilio/autoload.php'; // Loads the library
     require 'twilio-php-master/Services/Twilio.php';
     use Twilio\Rest\Client;
-    $account_sid = 'ACc2ee9a0b7bd01ddfd3079fc23a433407';
-    $auth_token = '6a7fed7f68a9d89d75996bc165982d2b';
+    $account_sid = '';
+    $auth_token = '';
     $client = new Client($account_sid, $auth_token);
 
 $number = $_POST['From'];
